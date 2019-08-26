7439c87ece81e37e07029f78244415d51bdda547
Paulo Cristovam <paulo.cristovam@desk35.ad.kognit.com.br>
2019-08-26 13:16:15 -0300

    update

---
+++ Camadas/2 -Domain/Aps19.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aps19.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserLogin { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public bool UserIsActive { get; set; }
+    }
+}
---
+++ Camadas/4 - Infra/4.2 - Data/Aps19.Infra.Data/Context/ApsContext.cs
@@ -0,0 +1,17 @@
+﻿using Aps19.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Aps19.Infra.Data.Context
+{
+    public class ApsConstext : DbContext
+    {
+        public ApsConstext() : base("ApiDDD") {
+
+        }
+
+        public DbSet<User> Usuarios { get; set; }
+    }
+}
